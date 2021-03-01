f70b3ffe12e37281c8cf0da67aeca5b2d07ef8f2
mfadam786 <mfadam786@gmail.com>
2021-03-01 14:45:29 +1300

    Setup urls, settings and ran the server

---
+++ JobSkillsMonitor/backend/urls.py
@@ -0,0 +1,11 @@
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from . import views
+
+# App name route
+app_name = "backend"
+
+# Project URL paths
+urlpatterns = [
+    path('', views.index),
+]

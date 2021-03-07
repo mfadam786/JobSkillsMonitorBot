df7a853445c6683685a9758d1f97f773c9b1ac7e
CodySmithZ <csmit444@gmail.com>
2021-03-08 12:42:44 +1300

    Restarted Models

--- JobSkillsMonitor/backend/admin.py
+++ JobSkillsMonitor/backend/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from backend.models import location, listing
 # Register your models here.
+admin.site.register(admin, location, listing)
--- JobSkillsMonitor/backend/models.py
+++ JobSkillsMonitor/backend/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class location(models.Model):
+    area = models.TextField(max_length=100)
+
+class listing(models.Model):
+  
+    job_type = models.TextField(max_length=100)
+    company = models.TextField(max_length=100)
+    job_title = models.TextField(max_length=100)
+    job_url = models.URLField()
+    date_listed = models.DateField()
+
+    location = models.ForeignKey(location, on_delete=models.CASCADE)
+

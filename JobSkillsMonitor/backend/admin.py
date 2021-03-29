90c6ed14b459a08180bfc692b2f5b9447b5af661
mfadam786 <mfadam786@gmail.com>
2021-03-29 13:43:41 +1300

    Read in csv and saved it to sqlite DB

--- JobSkillsMonitor/backend/admin.py
+++ JobSkillsMonitor/backend/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import location, listing
+from .models import Listing
 # Register your models here.
-admin.site.register(location)
-admin.site.register(listing)
+admin.site.register(Listing)
--- JobSkillsMonitor/backend/models.py
+++ JobSkillsMonitor/backend/models.py
@@ -2,22 +2,17 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 
-class location(models.Model):
-    name = models.TextField(unique=True, max_length=100, default="")
-
-class listing(models.Model):    
-    date_listed = models.DateField(default=timezone.now())
+class Listing(models.Model):    
+    date_listed = models.DateField(default=timezone.now()) #dd-mm-yyyy
     employer = models.TextField(max_length=100, default="")
     company = models.TextField(max_length=100, default="")
     work_type = models.TextField(max_length=15, default="")
     job_role = models.TextField(max_length=50, default="")
     data = models.TextField(default="")
 
-    region = models.ForeignKey('location', on_delete=models.CASCADE, default="")
+    region = models.TextField(max_length=50, default="")
 
-    city = models.TextField(max_length=50, default="")
+    sub_region = models.TextField(max_length=50, default="")
 
     job_title = models.TextField(max_length=100, default="")
-    
-    # job_type = models.TextField(max_length=100)
 

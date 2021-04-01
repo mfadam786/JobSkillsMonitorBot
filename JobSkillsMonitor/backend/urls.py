8a029fb314910c6826482b807866e7144c787afd
Cody Smith (1000060722) <SMITCR7@student.op.ac.nz>
2021-04-01 14:57:39 +1300

    Created sub region map

--- JobSkillsMonitor/backend/urls.py
+++ JobSkillsMonitor/backend/urls.py
@@ -9,5 +9,7 @@ app_name = "backend"
 urlpatterns = [
     path('', views.index, name="index"),
     path('store-data', views.store_data, name="store_data"),
-    path('test_maps', views.test_maps, name='test_maps')
+    path('test_maps/regions', views.test_maps_regions, name='test_maps_regions'),
+    path('test_maps/sub_regions', views.test_maps_sub_regions, name='test_maps_sub_region')
+
 ]

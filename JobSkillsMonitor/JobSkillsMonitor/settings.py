d7a4178065a9ec292646f482504f3ff54122505a
CodySmithZ <csmit444@gmail.com>
2021-04-08 14:55:31 +1200

    Updates postgresql details

--- JobSkillsMonitor/JobSkillsMonitor/settings.py
+++ JobSkillsMonitor/JobSkillsMonitor/settings.py
@@ -76,13 +76,17 @@ WSGI_APPLICATION = 'JobSkillsMonitor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'JobSkillMonitorBot',
+        'USER': 'JobBotGuest',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators

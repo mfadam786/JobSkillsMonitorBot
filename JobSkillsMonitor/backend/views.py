8a1ab806aa247719bcdec7b68fa64fea8f405f69
BrynGibson <brynisawesome@gmail.com>
2021-05-10 12:46:07 +1200

    fix tsne on dev

--- JobSkillsMonitor/backend/views.py
+++ JobSkillsMonitor/backend/views.py
@@ -224,24 +224,76 @@ def search(request):
 
         lang_count = {k:lang_count[k] for k in lang_count}
         job_pay = Job_Pay.objects.annotate(search=SearchVector('job_title')).filter(search='web')
-        
-        context = { 
-            'job_listing' : job_listings, 
-            'searched_job' : job_title, 
-            'job_count' : job_count, 
-            'geojson': location_data['geoJson'], 
+
+        context = {
+            'job_listing': job_listings,
+            'searched_job': job_title,
+            'job_count': job_count,
+            'geojson': location_data['geoJson'],
             'region_count': location_data['region_count'],
-            'work_type_data_labels' : work_type_data['labels'],
-            'work_type_data' : work_type_data['data'],
-            'work_type_data_display' : work_type_data['display'],
-            'regional_job_count_data_labels' : regional_job_count['labels'],
-            'regional_job_count_data' : regional_job_count['data'],
-            'regional_job_count_data_display' : regional_job_count['display'],
-            'seek_url' : seek_url,
-            'indeed_url' : indeed_url,
-            'job_pay': job_pay[0], 
+            'work_type_data_labels': work_type_data['labels'],
+            'work_type_data': work_type_data['data'],
+            'work_type_data_display': work_type_data['display'],
+            'regional_job_count_data_labels': regional_job_count['labels'],
+            'regional_job_count_data': regional_job_count['data'],
+            'regional_job_count_data_display': regional_job_count['display'],
+            'seek_url': seek_url,
+            'indeed_url': indeed_url,
+            'job_pay': job_pay[0],
             'lang_count': lang_count
         }
+
+        model = Word2Vec.load("../scaper/models/word2vec_bi_updated.model")
+
+        q = job_title
+
+        if len(q.split(" ")) > 1:
+            word = "_".join(q.split(" "))
+        else:
+            word = q
+
+
+
+        if word in model.wv.vocab:
+            arr = np.empty((0, 50), dtype='f')
+            word_labels = [word]
+
+            close_words = model.similar_by_word(word)
+            arr = np.append(arr, np.array([model[word]]), axis=0)
+            for wrd_score in close_words:
+                wrd_vector = model[wrd_score[0]]
+                word_labels.append(wrd_score[0])
+                arr = np.append(arr, np.array([wrd_vector]), axis=0)
+
+            tsne = TSNE(n_components=2, random_state=42)
+            np.set_printoptions(suppress=True)
+            Y = tsne.fit_transform(arr)
+            x_coords = Y[:, 0]
+            y_coords = Y[:, 1]
+            plt.scatter(x_coords, y_coords)
+            for label, x, y in zip(word_labels, x_coords, y_coords):
+                plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords='offset points')
+                plt.xlim(x_coords.min() + 0.00005, x_coords.max() + 0.00005)
+                plt.ylim(y_coords.min() + 0.00005, y_coords.max() + 0.00005)
+
+            fig = plt.gcf()
+            buff = io.BytesIO()
+            fig.savefig(buff, format="png")
+
+            buff.seek(0)
+            string = base64.b64encode(buff.read())
+
+            context["image"] = 'data:image/png;base64,' + urllib.parse.quote(string)
+
+            plt.clf()
+
+        else:
+            context["error"] = "error that word is not in the vocabulary"
+
+        if q:
+            context["q"] = q
+        
+
 
 
         return render(request, template_name, context)
